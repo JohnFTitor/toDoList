48dff007268e145afc788c177aa87f0fb1ebc10c
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-19 16:49:30 -0500

    Finish test for add method
    - Add enviornment to use document mock
    - Add condition to test

--- src/scripts/test/[..].test.js
+++ src/scripts/test/[..].test.js
@@ -1,9 +1,11 @@
-const { TestWatcher } = require('jest');
-const TaskCollection = require('../TaskCollection');
+import TaskCollection from '../TaskCollection.js';
 
 describe('testing add task', () => {
-   test('To add a task', () => {
-       const tasks = new TaskCollection();
-       tasks.addTask 
-   }) 
-})
+  test('To add a task with just text', () => {
+    document.body.innerHTML = `
+     <ul></ul>`;
+    const tasks = new TaskCollection();
+    tasks.addTask('Task 1');
+    expect(tasks.list).toContainEqual({ description: 'Task 1', completed: false, index: 1 });
+  });
+});
