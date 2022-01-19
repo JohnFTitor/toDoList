971ba41f84f1d4256b6325babd336c1cf0e73c61
oyelakinG9 <oyelakinr37@gmail.com>
2022-01-19 21:50:29 +0100

    Install jest, and add path to the test file

---
+++ src/scripts/test/[..].test.js
@@ -0,0 +1,9 @@
+const { TestWatcher } = require('jest');
+const TaskCollection = require('../TaskCollection');
+
+describe('testing add task', () => {
+   test('To add a task', () => {
+       const tasks = new TaskCollection();
+       tasks.addTask 
+   }) 
+})
